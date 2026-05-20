5c7683998bfc4ae251a02abb7e084d1cfb843324
Pontus Ivarsson <pontus.ivarsson@masarinconsulting.se>
2026-05-20 16:24:58 +0200

    GetTenantsWithAllowedScopes

--- internal/presentation/api/authz/context.go
+++ internal/presentation/api/authz/context.go
@@ -53,3 +53,38 @@ func HasTenantAccess(ctx context.Context, tenant string, scopes ...Scope) bool {
 
 	return true
 }
+
+// GetTenantsWithAllowedScopes extracts the names of allowed tenants, if any, from the provided context
+func GetTenantsWithAllowedScopes(ctx context.Context, scopes ...Scope) []string {
+	access, ok := ctx.Value(accessCtxKey).(accessMap)
+	requiredScopeCount := len(scopes)
+
+	if !ok || requiredScopeCount == 0 {
+		return []string{}
+	}
+
+	// If the required scope is AnyScope we set the scope count to
+	// 0 to disable the scope checking below
+	if requiredScopeCount == 1 && scopes[0] == AnyScope {
+		requiredScopeCount = 0
+	}
+
+	tenants := make([]string, 0, len(access))
+
+	for t, allowedScopes := range access {
+		idx := 0
+
+		for idx < requiredScopeCount {
+			if _, ok := allowedScopes[scopes[idx]]; !ok {
+				break
+			}
+			idx++
+		}
+
+		if idx == requiredScopeCount {
+			tenants = append(tenants, t)
+		}
+	}
+
+	return tenants
+}
